36c9c3e50a7c7c77637431fce5b83a324138385a
Colunga-D <xkould@gmail.com>
2024-02-07 13:56:15 +0100

    Headers end design and work

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import Header from './components/general/Header.vue';
+import Header from './components/general/Header.vue';
 import Footer from './components/general/Footer.vue';
-import HeaderEventVue from './components/general/HeaderEvent.vue';
+// import HeaderEventVue from './components/general/HeaderEvent.vue';
   
 </script>
 
 <template>
   <header>
     <!-- <Header /> -->
-    <HeaderEventVue />
+    <Header />
   
     <div class="wrapper">
 
