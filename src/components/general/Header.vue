c2a56fdf72e3fd49f92b7f4d2a39243a4d77f206
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2024-02-28 20:59:51 +0100

    Details page fixed.

--- src/components/general/Header.vue
+++ src/components/general/Header.vue
@@ -1,214 +1,238 @@
 <script setup>
 const showSidebar = () => {
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.style.display = 'flex';
-}
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "flex";
+};
 const hideSidebar = () => {
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.style.display = 'none';
-}
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "none";
+};
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Tu código JavaScript aquí
-    let prevScrollpos = window.scrollY;
-    const header = document.getElementById("header");
+document.addEventListener("DOMContentLoaded", function () {
+	// Tu código JavaScript aquí
+	let prevScrollpos = window.scrollY;
+	const header = document.getElementById("header");
 
-    window.addEventListener("scroll", function() {
-      let currentScrollPos = window.scrollY;
-      if (prevScrollpos > currentScrollPos) {
-        header.style.top = "0";
-      } else {
-        header.style.top = "-10rem";
-      }
-      prevScrollpos = currentScrollPos;
-    });
+	window.addEventListener("scroll", function () {
+		let currentScrollPos = window.scrollY;
+		if (prevScrollpos > currentScrollPos) {
+			header.style.top = "0";
+		} else {
+			header.style.top = "-10rem";
+		}
+		prevScrollpos = currentScrollPos;
+	});
 });
-
 </script>
 
 <template>
-  <header id="header">
+	<header id="header">
+		<div id="logo_container">
+			<img src="./../../assets/images/logoConectados.png" alt="Logo" />
+		</div>
 
-      <div id="logo_container">
-        <img src="./../../assets/images/logoConectados.png" alt="Logo">
-      </div>
-
-      <nav>
-        <div class="sidebar">
-          <li @click="hideSidebar"><img src="./../../assets/icons/hide-hamburger.svg" alt="Logo"></li>
-          <img src="./../../assets/images/logoConectados-pink.png" alt="Logo">
-          <router-link to="/" class="button underline">Home</router-link>
-          <router-link to="/my-events" class="button underline">My Events</router-link>
-          <router-link to="/control-panel" class="button underline">Control Panel</router-link>
-          <router-link to="/login" class="button login_button">Log In</router-link>
-        </div>
-        <div id="desktop-menu">
-          <router-link to="/" class="button underline hideOnMobile">Home</router-link>
-          <router-link to="/my-events" class="button underline hideOnMobile">My Events</router-link>
-          <router-link to="/control-panel" class="button underline hideOnMobile">Control Panel</router-link>
-          <router-link to="/login" class="button hideOnMobile login_button">Log In</router-link>
-          <li @click="showSidebar"><img src="./../../assets/icons/hamburger-menu.svg" alt="Logo"></li>
-        </div>
-      </nav>
-  </header>
+		<nav>
+			<div class="sidebar">
+				<li @click="hideSidebar">
+					<img
+						src="./../../assets/icons/hide-hamburger.svg"
+						alt="Logo"
+					/>
+				</li>
+				<img
+					src="./../../assets/images/logoConectados-pink.png"
+					alt="Logo"
+				/>
+				<router-link to="/" class="button underline">Home</router-link>
+				<router-link to="/my-events" class="button underline"
+					>My Events</router-link
+				>
+				<router-link to="/control-panel" class="button underline"
+					>Control Panel</router-link
+				>
+				<router-link to="/login" class="button login_button"
+					>Log In</router-link
+				>
+			</div>
+			<div id="desktop-menu">
+				<router-link to="/" class="button underline hideOnMobile"
+					>Home</router-link
+				>
+				<router-link
+					to="/my-events"
+					class="button underline hideOnMobile"
+					>My Events</router-link
+				>
+				<router-link
+					to="/control-panel"
+					class="button underline hideOnMobile"
+					>Control Panel</router-link
+				>
+				<router-link
+					to="/login"
+					class="button hideOnMobile login_button"
+					>Log In</router-link
+				>
+				<li @click="showSidebar">
+					<img
+						src="./../../assets/icons/hamburger-menu.svg"
+						alt="Logo"
+					/>
+				</li>
+			</div>
+		</nav>
+	</header>
 </template>
 
-
 <style lang="scss" scoped>
-
 header {
-  height: 10rem;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px 6rem;
-  background-color: rgba(166, 166, 166, 0.214);
-  backdrop-filter: blur(30px);
-  position: fixed;
-  z-index: 998;
+	height: 10rem;
+	width: 100vw;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0px 6rem;
+	background-color: rgba(0, 0, 0, 0.214);
+	backdrop-filter: blur(30px);
+	position: fixed;
+	z-index: 998;
 }
 
 #logo_container {
-  width: 20rem;
+	width: 20rem;
 
-  img {
-    width: 100%;
-  }
+	img {
+		width: 100%;
+	}
 }
 #desktop-menu {
-  font-size: 1.7rem;
-  font-family: 'Poppins', sans-serif;
-  display: flex;
-  gap: 3rem;
-  align-items: center;
-  justify-content: space-between;
-  align-items: center;
+	font-size: 1.7rem;
+	font-family: "Poppins", sans-serif;
+	display: flex;
+	gap: 3rem;
+	align-items: center;
+	justify-content: space-between;
+	align-items: center;
 
-  li {
-    list-style: none;
-    display: none;
-    img {
-      width: 2.3em;
-    }  
-  } 
-  .button {
-    color: white;
-    text-decoration: none;
-  }
+	li {
+		list-style: none;
+		display: none;
+		img {
+			width: 2.3em;
+		}
+	}
+	.button {
+		color: white;
+		text-decoration: none;
+	}
 
-  .login_button {
-  border-radius: 0.5rem;
-  border: solid 0.1rem white;
-  padding: 1rem 3rem;
-  transition: 300ms ease-in-out;
-  }
-  .login_button:hover {
-    background-color: white;
-    color: black;
-  }
+	.login_button {
+		border-radius: 0.5rem;
+		border: solid 0.1rem white;
+		padding: 1rem 3rem;
+		transition: 300ms ease-in-out;
+	}
+	.login_button:hover {
+		background-color: white;
+		color: black;
+	}
 }
 
 .sidebar {
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100vh;
-  width: 100%;
-  z-index: 999;
-  background-color: white;
-  box-shadow: -10px 0 10px rgba(0, 0, 0, 0.244);
-  display: none;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem 0;
-  gap: 4vh 0;
-  display: none;
+	position: fixed;
+	top: 0;
+	right: 0;
+	height: 100vh;
+	width: 100%;
+	z-index: 999;
+	background-color: white;
+	box-shadow: -10px 0 10px rgba(0, 0, 0, 0.244);
+	display: none;
+	flex-direction: column;
+	align-items: center;
+	padding: 2rem 0;
+	gap: 4vh 0;
+	display: none;
 
-  .login_button {
-  border-radius: 0.5rem;
-  border: solid 0.1rem #ad8799;
-  padding: 1rem 3rem;
-  transition: 300ms ease-in-out;
-  }
-  .login_button:hover {
-    background-color: #ad8799;
-    color: rgb(255, 255, 255);
-  }
+	.login_button {
+		border-radius: 0.5rem;
+		border: solid 0.1rem #ad8799;
+		padding: 1rem 3rem;
+		transition: 300ms ease-in-out;
+	}
+	.login_button:hover {
+		background-color: #ad8799;
+		color: rgb(255, 255, 255);
+	}
 
-  .button {
-    color: rgb(0, 0, 0);
-    text-decoration: none;
-    font-size: 2rem;
-    font-family: 'Poppins', sans-serif;
-  }
-  
-  > img {
-    width: 80%;
-    margin-bottom: 4rem;
-  }
+	.button {
+		color: rgb(0, 0, 0);
+		text-decoration: none;
+		font-size: 2rem;
+		font-family: "Poppins", sans-serif;
+	}
 
-  li{
-    width: 100%;
-    display: flex;
-    margin-left: 5rem;
+	> img {
+		width: 80%;
+		margin-bottom: 4rem;
+	}
 
-    img {
-    width: 3em;
-    height: 90%;
+	li {
+		width: 100%;
+		display: flex;
+		margin-left: 5rem;
 
-  }
+		img {
+			width: 3em;
+			height: 90%;
+		}
+	}
+
+	.button {
+		color: black;
+	}
+	.button:hover {
+		color: black;
+	}
 }
-
-  .button {
-    color: black;
-  }
-  .button:hover {
-    color: black;
-  }
-}
-
-
 
 .underline {
-  display: inline-block;
-
+	display: inline-block;
 }
 .underline:after {
-content: '';
-  width: 0px;
-  height: 0.1rem;
-  display: block;
-  background: white;
-  transition: 300ms ease-in-out;
+	content: "";
+	width: 0px;
+	height: 0.1rem;
+	display: block;
+	background: white;
+	transition: 300ms ease-in-out;
 }
 .underline:hover:after {
-  width: 100%;
+	width: 100%;
 }
 
-#stickyheaders{
-transition : transform 0.34s ease;
+#stickyheaders {
+	transition: transform 0.34s ease;
 }
-.headerup{
-transform: translateY(-110px); 
+.headerup {
+	transform: translateY(-110px);
 }
 
 #header {
-  transition: top 0.3s ease;
+	transition: top 0.3s ease;
 }
 
-@media (max-width: 800px){
-  .hideOnMobile {
-    display: none;
-  }
-  
-  header {
-  padding: 0px 3rem;
-  }
+@media (max-width: 800px) {
+	.hideOnMobile {
+		display: none;
+	}
 
-  #desktop-menu > li {
-    display: block;
-  } 
+	header {
+		padding: 0px 3rem;
+	}
+
+	#desktop-menu > li {
+		display: block;
+	}
 }
-
 </style>
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,88 +1,128 @@
 <script setup>
-import HeaderVue from './../components/general/Header.vue';
-import FooterVue from './../components/general/Footer.vue';
-import { useContentStore } from '@/stores/ContentStore';
-import { ref, watchEffect  } from 'vue';
+import HeaderVue from "./../components/general/Header.vue";
+import FooterVue from "./../components/general/Footer.vue";
+import { useContentStore } from "@/stores/ContentStore";
+import { ref, watchEffect } from "vue";
 
 const props = defineProps({
-  id_event: String,
+	id_event: String,
 });
 
 const contentStore = useContentStore();
 const content = ref(null);
 
-console.log(contentStore);
-
 watchEffect(() => {
-  if (props.id_event) {
-    contentStore.fetchContentById(props.id_event).then(() => {
-      content.value = contentStore.content;
-    });
-  }
+	if (props.id_event) {
+		contentStore.fetchContentById(props.id_event).then(() => {
+			content.value = contentStore.content;
+		});
+	}
 });
 
+console.log(contentStore.content);
 </script>
 <template>
-    <HeaderVue /> 
-    <main>  
-        <div class="title">
-            <h2>{{ content.name }}</h2>
-        </div>
-        <div class="container_elements">
-            <p>Lugar: {{ content.ubication }}</p>
-            <p>Plazas disponibles: {{ content.capacity }}</p>
-            <p>Fecha: {{ content.date }}</p>
-            <p>Precio: ¥{{ content.price }}</p>
-            <h4>Detalles:</h4>
-            <p>{{ content.description }}</p>
+	<main>
+		<div
+			:style="{
+				'background-image': 'url(' + contentStore.content.image + ')',
+			}"
+			id="event_image"
+			v-if="content"
+		>
+			<div class="title" v-if="content">
+				<h1>{{ contentStore.content.name }}</h1>
+			</div>
+		</div>
+		<article v-if="content">
+			<div>
+				<img src="./../assets/icons/event-icon-place.svg" alt="" />
+				<p>Lugar: {{ contentStore.content.ubication }}</p>
+			</div>
+			<div>
+				<img src="./../assets/icons/event-icon-seats.svg" alt="" />
 
-            <button class="subscribe_event">Suscribete al evento</button>
-        </div>
-        <div v-if="loading">
-            Cargando...
-        </div>
-        <div v-if="error">
-            Ha ocurrido un error: {{ error.message }}
-        </div>
-    </main>
-    <FooterVue /> 
+				<p>Plazas disponibles: {{ contentStore.content.capacity }}</p>
+			</div>
+			<div>
+				<img src="./../assets/icons/event-icon-date.svg" alt="" />
+				<p>Fecha: {{ contentStore.content.date }}</p>
+			</div>
+			<div>
+				<img src="./../assets/icons/event-icon-ticket.svg" alt="" />
+				<p>Precio: ¥{{ contentStore.content.price }}</p>
+			</div>
+			<section id="details">
+				<h4>Detalles:</h4>
+				<p>{{ contentStore.content.description }}</p>
+			</section>
+
+			<button>Suscribete al evento</button>
+		</article>
+		<div v-if="loading">Cargando...</div>
+		<div v-if="error">Ha ocurrido un error: {{ error.message }}</div>
+	</main>
 </template>
-<style scoped lang="scss">
-p, .subscribe_event{
-    font-size: 1.8rem;
-    font-family: 'Poppins', sans-serif;
-}
-h4{
-    font-size: 2rem;
-    font-family: 'Poppins', sans-serif;
-}
-main{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 2rem;
-}
-.title{
-    text-align: center;
-}
-.subscribe_event{
-    background-color: #D64747;
-    color: #000;
-    padding: 1rem;
-    text-align: center;
-    border: none;
-    border-radius: 10px;
-}
-.container_elements{
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-    border: 1px solid #000;
-    border-radius: 10px;
 
-    p, h4{
-        margin-left: 1rem;
-        margin-right: 1rem;
-    }
+<style scoped lang="scss">
+#event_image {
+	padding-top: 10rem;
+	height: 80vh;
+	width: 100%;
+	background-size: cover;
+	background-position: top;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	h1 {
+		text-align: center;
+		font-family: "Poppins", sans-serif;
+		font-size: calc(16px + 3vw);
+		font-weight: 400;
+		color: white;
+		padding: 2rem;
+		filter: drop-shadow(0 0 0.75rem rgb(0, 0, 0));
+	}
+}
+
+article {
+	font-family: "Poppins", sans-serif;
+	font-size: 2rem;
+	margin: 5vw;
+	padding: 2.5vh;
+	border: 1px solid rgb(210, 210, 210);
+	border-radius: 2rem;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	div {
+		display: flex;
+		gap: 1rem;
+		img {
+			height: 3rem;
+		}
+		align-items: center;
+	}
+}
+
+button {
+	padding: 2rem 1rem;
+	border: none;
+	border-radius: 1rem;
+	background-color: #ff8080;
+	color: white;
+	font-size: 2rem;
+	transition: ease-in-out 0.3s;
+}
+
+button:hover {
+	background-color: #5097a6;
+}
+
+@media (min-width: 500px) {
+	article {
+		margin: 10vh 10vw;
+		padding: 5vh;
+	}
 }
 </style>
