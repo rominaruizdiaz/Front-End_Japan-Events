68379bd7a4df1da4898ac47b43433ce81c32a398
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2024-02-28 10:16:29 +0100

    header done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,11 @@ console.log(store.getEvents())
 <template>
   <body>
     <HeaderVue />
-    <main>
+    <div id="header-image">
+      <img src="./../assets/images/imagenFondoHeader.jpg" alt="">
+    </div>
+<!-- 
+     <main>
       <section>
         <div>
           <CardFilter />
@@ -30,78 +34,86 @@ console.log(store.getEvents())
         </div>
       </section>
     </main>
-    <FooterVue />
+    <FooterVue /> -->
   </body>
   
 </template>
 
 
 <style scoped lang="scss">
+#header-image {
+  height: 30vh;
+  width: 100vw;
 
-section {
-  background-image: url(./../assets/images/background-cards.png);
-  background-repeat: no-repeat;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  > div {
-    width: 90%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 3vh;
+  img {
+    object-fit: cover;
+    width: 100%;
   }
 }
+// section {
+//   background-image: url(./../assets/images/background-cards.png);
+//   background-repeat: no-repeat;
+//   background-size: cover;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
 
-#cards_container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: column;
-    gap: 3vh;
-}
+//   > div {
+//     width: 90%;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     flex-direction: column;
+//     gap: 3vh;
+//   }
+// }
 
-@media (min-width: 500px) {
-#cards_container {
-  display: grid;
-  grid-template-columns: auto;
-}
-}
+// #cards_container {
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-between;
+//     flex-direction: column;
+//     gap: 3vh;
+// }
 
-@media (min-width: 800px) {
-#cards_container {
-  grid-template-columns: auto auto;
-}
-}
-@media (min-width: 1200px) {
-  section {
+// @media (min-width: 500px) {
+// #cards_container {
+//   display: grid;
+//   grid-template-columns: auto;
+// }
+// }
 
-    > div {
-      width: 70%;
-    }
-  }
-#cards_container {
-    gap: 5vh;
-    display: grid;
-    grid-template-columns: auto auto auto auto;
-}
+// @media (min-width: 800px) {
+// #cards_container {
+//   grid-template-columns: auto auto;
+// }
+// }
+// @media (min-width: 1200px) {
+//   section {
 
-}
-@media (min-width: 1600px) {
-  section {
+//     > div {
+//       width: 70%;
+//     }
+//   }
+// #cards_container {
+//     gap: 5vh;
+//     display: grid;
+//     grid-template-columns: auto auto auto auto;
+// }
 
-    > div {
-      width: 70%;
-    }
-  }
-#cards_container {
-    gap: 5vh;
-    display: grid;
-    grid-template-columns: auto auto auto auto;
-}
+// }
+// @media (min-width: 1600px) {
+//   section {
 
-}
+//     > div {
+//       width: 70%;
+//     }
+//   }
+// #cards_container {
+//     gap: 5vh;
+//     display: grid;
+//     grid-template-columns: auto auto auto auto;
+// }
+
+// }
 </style>
